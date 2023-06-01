d11efdd1b88b8b6ba16982af77e98c4480053d46
David Duarte <77313412+davemmWeb@users.noreply.github.com>
2023-05-31 22:31:32 -0500

    correction ok

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -47,7 +47,7 @@ const About: React.FC<AboutProps> = () => {
 					My name is David Duarte, software engineer and fullstack developer with more than 4 years
 					of experience in web application development solutions, advanced knowledge in Html, Css,
 					JavaScript, Vue, Vuex, Next, Node, Express, React, Redux I also have With experience in
-					relational databases MySQL, PostgreSQL, Sequelice and management of GitHub version controllers.
+					relational databases MySQL, PostgreSQL, Sequelize and management of GitHub version controllers.
 					I focus on solving problems and I love teamwork. I also like to collaborate on interface design.
 				</p>
 			</div>
